44c4c3aeafc1f19badd220ef63a81b89aab7ae16
Masud Rahman <masud.rahman@dal.ca>
2021-04-18 19:26:49 -0300

    Corrected the typo!

--- src/strict/ca/usask/cs/srlab/strict/test/ScannielloTest.java
+++ src/strict/ca/usask/cs/srlab/strict/test/ScannielloTest.java
@@ -6,12 +6,12 @@ import scanniello.method.LucenePRSearcher;
 import scanniello.method.RepoPRRankMaker;
 import strict.utility.MiscUtility;
 
-public class ScannieloTest {
+public class ScannielloTest {
 	
 	
 
 	@Test
-	public void getScannieloResults() {
+	public void getScannielloResults() {
 		String repoName = "adempiere-3.1.0";
 		String searchQuery = "LandedCost Cost Distribution type Cost work landed cost form select Costs cost distribution type working generating Landed cost allocation records Landed Cost";
 		int bugID = 815;
@@ -22,7 +22,7 @@ public class ScannieloTest {
 	
 	
 	@Test
-	public void getScannieloRanks() {
+	public void getScannielloRanks() {
 		String repoName="mahout-0.4";
 		boolean useBaseline=true;
 		RepoPRRankMaker maker=new RepoPRRankMaker(repoName, useBaseline);
